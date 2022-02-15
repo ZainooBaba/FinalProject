556dbfdcb1571ce6d6afc507dbcc5d13c32dc8fa
Arjun Gopal <arjun.gopal84@gmail.com>
2022-02-14 19:09:43 -0600

    changed text

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>YO</Text>
+      <Text>Arjun is Goated at everything</Text>
       <StatusBar style="auto" />
     </View>
   );
