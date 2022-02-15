501cdd6de2f3f6746dbf255dc83cae2f8eecccdc
ZainooBaba <34255867+ZainooBaba@users.noreply.github.com>
2022-02-14 19:13:10 -0600

    test commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Arjun is Goated at everything</Text>
+      <Text>Mr patel should have won vice-presidency</Text>
       <StatusBar style="auto" />
     </View>
   );
